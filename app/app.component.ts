15851bbc81981cf49cb66d52d8de0b4545a6f40d
jmllegrand <jml.legrand@gmail.com>
2017-01-08 11:35:50 +0100

    feat: first component (decorator, interpolation, inline template)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
+// Component is a decorator
+
+/**
+ * Component decorator allows you to mark a class as an Angular component and provide additional
+ * metadata that determines how the component should be processed, instantiated and used at
+ * runtime.
+ */
+
+// Component is just a function
+// Component()
+// We prefix it with the @ as this is a decorator to tell angular to register it as an Component
+// usage of backtick (es6 features)
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+     {{title}}
     </div>
   `
 })
+
+// use inline template
+// interpolation : all properties expose in the class are available in the template
+
 export class AppComponent {
 
+  // declare the property
+  title: string;
+
+  // we set value of the property 'title' in the constructor
+  constructor() {
+    this.title = "Ultimate angular";
+  }
 }
